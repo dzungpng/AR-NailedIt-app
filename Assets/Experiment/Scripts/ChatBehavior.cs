4cf2e8cd9abedd94622af5fc3be8fcbf605862f2
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-07-10 13:48:55 -0400

    Mirror networking chatbox works

---
+++ Assets/Experiment/Scripts/ChatBehavior.cs
@@ -0,0 +1,57 @@
+﻿using Mirror;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatBehavior : NetworkBehaviour
+{
+    [SerializeField] private Text chatText = null;
+    [SerializeField] private InputField inputField = null;
+    [SerializeField] private GameObject canvas = null;
+
+    private static event Action<string> OnMessage;
+
+    public override void OnStartAuthority()
+    {
+        canvas.SetActive(true);
+
+        OnMessage += HandleNewMessage;
+    }
+
+    [ClientCallback]
+    private void OnDestroy()
+    {
+        if(!hasAuthority) { return; }
+
+        OnMessage -= HandleNewMessage;
+    }
+
+    private void HandleNewMessage(string message)
+    {
+        chatText.text += message;
+    }
+
+    [Client]
+    public void Send()
+    {
+        if(!Input.GetKeyDown(KeyCode.Return)) { return; }
+        if (string.IsNullOrWhiteSpace(inputField.text)) { return; }
+        CmdSendMessage(inputField.text);
+        inputField.text = string.Empty;
+    }
+
+    [Command]
+    private void CmdSendMessage(string message)
+    {
+        // Validate message
+        RpcHandleMessage($"[{connectionToClient.connectionId}]: {message}");
+    }
+
+    [ClientRpc]
+    private void RpcHandleMessage(string message)
+    {
+        OnMessage?.Invoke($"\n{message}");
+    }
+
+
+}
---
+++ Assets/Experiment/Scripts/RandomSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += new Vector3(
+            Random.Range(-4f, 4f),
+            Random.Range(-4f, 4f),
+            0f
+        );
+    }
+}
