97fd65f6d43f00288d1e2f051e0078c21fca7d1b
tushAR <purangt@gmail.com>
2020-08-14 11:03:36 -0400

    initial commit

---
+++ Hololens_App_Tushar/Hololens lpms/Assets/Vuforia/DefaultTrackableEventManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultTrackableEventManager : MonoBehaviour
+{
+    public bool EventTrackable;
+    public SensorManager Tracker;
+    // Update is called once per frame
+    void Update()
+    {
+#if !UNITY_EDITOR
+        if (EventTrackable)
+        {
+           
+            Tracker.DrillSensor.transform.rotation = this.transform.rotation;
+            
+        }
+        else
+        {
+           
+            Tracker.HandleSensor.transform.rotation = this.transform.rotation;
+            
+        }
+#endif
+    }
+}
