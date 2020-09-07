735a3a394a92bb63eacc583e8fc7224b3df96d9b
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-09-07 16:03:16 -0400

    Integrated the app but settings are off and can't deploy

---
+++ Assets/_Scripts/HoloLens_Scripts/Scene for varying length nail/StepManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepManager : MonoBehaviour
+{
+
+    public GameObject IncissionAnimObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IncissionAnimObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Enable incission circle animation object
+            IncissionAnimObject.SetActive(true);
+            IncissionAnimObject.GetComponent<IncissionAnim>().gameObject.transform.position = 
+                IncissionAnimObject.GetComponent<IncissionAnim>().startPos.transform.position;
+        }
+    }
+}
